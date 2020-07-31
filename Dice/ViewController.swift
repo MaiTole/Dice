c06e218d2259bb7a28474bfc9fe84db99d123c53
Swati Vilas Tole <swatitole@Swatis-MacBook.local>
2020-07-31 22:58:28 +0800

    Dice App

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func rollDice(_ sender: Any) {
+        
+        let x = (1...6)
+        let number = Int(arc4random_uniform(UInt32(x.count)))
+        self.diceImageView.image = UIImage(named: "dice-\(number)")
     }
 
-
 }
+
 
